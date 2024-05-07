4ffcee902f08a7cbd0c23c79a60adbf3531d9fe6
Eric Raslich <rasliche@gmail.com>
2024-05-07 12:31:17 -0400

    reinstall packages after upgrades

--- pages/trivia/passport.vue
+++ pages/trivia/passport.vue
@@ -24,14 +24,16 @@
       <input class="block w-2/3 mx-auto border p-2 rounded" v-model="code" placeholder="M4NG" />
       <NuxtLink :to=questionCodeExists :disabled=questionCodeExists :class="[questionCodeExists ? 'bg-green-200 hover:bg-green-300 border-green-500 border-l-2-transparent border-t-2-transparent border-b-2 border-r-2 cursor-pointer' : 'bg-yellow-200 border-2 border-transparent cursor-not-allowed']" class="block w-1/4 mx-auto rounded p-2 mt-4 cursor-pointer">{{ questionCodeExists ? "Found!" : "Nothing yet..." }}</NuxtLink>
     </div>
+
     <section>
       <h3 class="w-full text-2xl">Stamps</h3>
       <div id="stamp-grid" class="grid gap-2 justify-items-center grid-cols-2 justify-center">
-        <div v-for="question in questionRoutes" class="w-24 h-24 bg-red-300 text-center">
+        <div v-for="question in questionRoutes" class="w-24 h-24 bg-red-300 text-center rounded">
           {{ question.meta.stamp }}
         </div>
       </div>
     </section>
+    
     <div class="m-24 p-2 border border-red-200">
       <h3>DEBUG</h3>
       {{ availableCodes }}
